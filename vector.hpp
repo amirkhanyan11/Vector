07038959f7321860acbacd28106c171a5e0eab69
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2023-12-25 16:42:08 +0400

    hehe

--- vector.hpp
+++ vector.hpp
@@ -121,11 +121,6 @@ namespace juju
 
     public:
 
-        // Iterators
-
-        //class iterator;
-        //class const_iterator;
-
         iterator begin() const noexcept;
         iterator end() const noexcept;
         const_iterator cbegin() const noexcept;
@@ -140,59 +135,6 @@ namespace juju
 
         void _resize();
     };
-
-
-
-
-    // template <typename T>
-    // class vector<T>::iterator
-    // {
-
-    // public:
-
-    //     friend class const_iterator;
-    //     iterator() = default;
-    //     iterator(T*);
-    //     iterator(const iterator&) = default;
-    //     iterator(const vector<T>&);
-        
-    // public:
-
-    //     iterator& operator++() noexcept;
-    //     iterator operator++(int) noexcept;
-    //     iterator& operator--() noexcept;
-    //     iterator operator--(int) noexcept;
-    //     T& operator*() const;
-    //     bool operator==(const iterator& other) const;
-    //     bool operator!=(const iterator& other) const;
-
-    // private:
-    //     T* m_ptr = nullptr;
-    // };
-
-
-
-    // template <typename T>
-    // class vector<T>::const_iterator
-    // {
-
-    // public:
-    //     const_iterator() = default;
-    //     const_iterator(const T*);
-    //     const_iterator(const vector<T>::iterator& other) : m_ptr{other.m_ptr} {};
-    //     const_iterator(const const_iterator& other) = default;
-    //     const_iterator& operator++() noexcept;
-    //     const_iterator operator++(int) noexcept;
-    //     const_iterator& operator--() noexcept;
-    //     const_iterator operator--(int) noexcept;
-    //     const_iterator& operator=(const iterator& other);
-    //     const T& operator*() const;
-    //     bool operator==(const const_iterator& other) const;
-    //     bool operator!=(const const_iterator& other) const;
-        
-    // private:
-    //     const T* m_ptr = nullptr;
-    // };
 
 }
 
